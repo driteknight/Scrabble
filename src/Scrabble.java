7a5c30b49e67af47f64d92c5096746f27d874c20
Vivek Gupta <vivekgupta@paypal.com>
2016-08-05 12:19:34 +0530

    First Commit

---
+++ src/Scrabble.java
@@ -0,0 +1,22 @@
+import java.util.Set;
+
+public class Scrabble {
+	
+	private static Set<String> allWords;
+	
+	private boolean isWord(String word) {
+		return allWords.contains(word);
+	}
+	
+	
+	
+	private int getScore(String str) {
+		return null;
+	}
+	
+	private 
+	
+	public static void main(String args[]) {
+		
+	}
+}
